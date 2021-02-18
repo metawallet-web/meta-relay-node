ef2ce472adaccfc0061ea640fee540ceca31edfe
andy <andy@jonezie.com>
2021-02-18 17:47:22 -0500

    adding libraries

--- bot.js
+++ bot.js
@@ -11,12 +11,12 @@ var INFURA_MAINNET_URL = 'https://mainnet.infura.io/gmXEVo5luMPUGPqg6mhy';
 var Web3 = require('web3')
 
 
-const relayConfig = require('./config/relay.config')
-const accountConfig = require('./config/account.config')
+const relayConfig = require('./config/relay.config.json')
+const accountConfig = require('./config/account.config.json')
 
 var mongoInterface = require('./lib/mongo-interface')
 
-var metaPeerInterface = require('./lib/meta-peer-interface')
+var apiInterface = require('./lib/api-interface')
 
 
 
@@ -25,9 +25,9 @@ init()
 async function init(){
     console.log('Booting relay node.')
 
-    await mongoInterface.init()
+    await mongoInterface.init('metarelay')
 
     var web3 = new Web3()
 
-    await metaPeerInterface.init(web3)
+    await apiInterface.init(web3,mongoInterface)
 }
